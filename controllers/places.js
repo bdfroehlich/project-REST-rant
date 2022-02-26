39443ab18df3d58d620b45bc1af082f91a66b3af
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-02-26 14:49:19 -0600

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -8,9 +8,20 @@ router.get('/', (req, res) => {
 
 // POST /places
 router.post('/', (req, res) => {
-  //console.log the body of the request on the backend server to check
-  console.log(req.body)
-  res.send('POST /places')
+  //check that post is making it to backend server
+  // console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 // GET /places/new
