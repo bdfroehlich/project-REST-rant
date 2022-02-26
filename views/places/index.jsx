c113bf39b270f41d74804346e8fcbd5496185625
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-02-26 08:40:53 -0600

    adding places page

---
+++ views/places/index.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const Default = require('../default');
+
+function places(){
+    return(
+        <Default>
+            <main>
+                <h1>PLACES</h1>
+            </main>
+        </Default>
+    )
+}
+
+module.exports = places;
