2792ef26486b61408e76c4561819c292c3b91124
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-02-26 09:10:10 -0600

    adding places view with mock data

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,14 +1,38 @@
 const React = require('react');
 const Default = require('../default');
 
-function places(){
-    return(
-        <Default>
-            <main>
-                <h1>PLACES</h1>
-            </main>
-        </Default>
-    )
-}
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div>
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}></img>
+        </div>
+      )
+    })
+    return (
+      <Default>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Default>
+  )
+  }
 
-module.exports = places;
+
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'http://placekitten.com/250/250'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'http://placekitten.com/250/250'
+  }]
+
+module.exports = index;
