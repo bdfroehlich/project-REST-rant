291b3765bec50c6d9bac9a7d0ac269ed24c081d1
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-02-26 11:20:44 -0600

    adding styles to rest-rant

--- views/home.jsx
+++ views/home.jsx
@@ -4,9 +4,12 @@ const Default = require('./default');
 function home () {
     return (
       <Default>
-        //will be placed inside of the body tag within the Default tag found in default.jsx
+        {/* will be placed inside of the body tag within the Default tag found in default.jsx */}
           <main>
               <h1>HOME</h1>
+              <a href="/places">
+                <button className="btn-primary">Places Page</button>
+              </a>
           </main>
       </Default>
     )
