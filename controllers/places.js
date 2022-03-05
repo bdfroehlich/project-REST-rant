0e27f056fa96f4905ab4ec25cb30649d2953a9a1
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-03-05 14:05:19 -0600

    refactoring to use mongoose, updating form styling

--- controllers/places.js
+++ controllers/places.js
@@ -1,109 +1,61 @@
 const router = require('express').Router()
-const places = require('../models/places')
+const db = require('../models')
 
-// GET /places
 router.get('/', (req, res) => {
+  db.Place.find()
+  .then((places) => {
     res.render('places/index', {places})
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
 })
 
-// POST /places
 router.post('/', (req, res) => {
-  //check that post is making it to backend server
-  // console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
-// GET /places/new
 router.get('/new', (req, res) => {
-    res.render('places/new')
+  res.render('places/new')
 })
 
-//Show /places/:id
 router.get('/:id', (req, res) => {
-  // casting the id to a number
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    //pass places[id] into view
-    res.render('places/show', { place: places[id], id: id })
-  }
-})
-
-//Delete places/:id
-//already in the places controller do not need /places/:id just need /:id
-router.delete('/:id', (req, res) => {
-  console.log(req.body)
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
-})
-
-// EDIT
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
       res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id], id: id })
-  }
+  })
 })
-
-// UPDATE - define put route
 
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  //is this number inside of our index
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      // if (!req.body.pic) {
-      //     // Default image if one is not provided
-      //     req.body.pic = 'http://placekitten.com/400/400'
-      // }
-      // if (!req.body.city) {
-      //     req.body.city = 'Anytown'
-      // }
-      // if (!req.body.state) {
-      //     req.body.state = 'USA'
-      // }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
+  res.send('PUT /places/:id stub')
 })
 
-module.exports = router;
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,11 +2,11 @@ const React = require('react');
 const Default = require('../default');
 
 function index (data) {
-    let placesFormatted = data.places.map((place, index) => {
+    let placesFormatted = data.places.map((place) => {
       return (
         <div key={place.name} className="col">
           <h2 className="text-center">
-          <a href={`/places/${index}`} >
+          <a href={`/places/${place.id}`} >
             {place.name}
           </a>
           </h2>
