23522dc34ca2027fbfa046500579362be6b764e3
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-03-07 18:36:13 -0600

    updating styling on new

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -9,28 +9,28 @@ function New () {
                     Add a New Place
                 </h1>
                 <form method="POST" action="/places">
-                    <div className="row">
-                    <div className="col-sm-6 col-md-4 col-lg-3">
+                    <div className="form-row">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="name">Place Name</label>
                         <input className="form-control" id="name" name="name" required />
                     </div>
-                    <div className="col-sm-6 col-md-4 col-lg-3">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="pic">Place Picture</label>
                         <input className="form-control" id="pic" name="pic" />
                     </div>
-                    <div className="col-sm-6 col-md-4 col-lg-3">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="city">City</label>
                         <input className="form-control" id="city" name="city" />
                     </div>
-                    <div className="col-sm-6 col-md-4 col-lg-3">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="state">State</label>
                         <input className="form-control" id="state" name="state" />
                     </div>
-                    <div className="col-sm-6 col-md-4 col-lg-3">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" required />
                     </div>
-                    <div className="col-sm-6 col-md-4 col-lg-3">
+                    <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label for="founded">Founded Year</label>
                         <input className="form-control" id="founded" name="founded" required />
                     </div>
