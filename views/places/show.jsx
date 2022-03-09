7e586c65b5d31644c6795596568def8f4fd13058
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-03-09 16:16:28 -0600

    added instance method showEstablished()

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,29 +5,34 @@ function show ({place, id}) {
     return (
         <Default>
           <main>
+            <div className="row">
             <h1>Show Page</h1>
             <h1>{place.name}</h1>
-            <div>
+            <div className="col-sm-6">
               <img className="rounded mx-auto d-block" src={place.pic} alt={place.name} />
+              <h3 className="text-center">
+                Located in {place.city}, {place.state}
+              </h3>
             </div>
-            <div>
+            <div className="col-sm-6">
               <h1>Rating</h1>
               <p className="text-center">
                 Not Rated
               </p>
-              <h1>Description</h1>
-              <p className="text-center">
-                Located in {place.city}, {place.state}
-              </p>
-              <p className="text-center">
-                Available Cuisines: {place.cuisines}
-              </p>
+              <h2>Description</h2>
+              <h3>
+                {place.showEstablished()}
+              </h3>
+              <h4 className="text-center">
+                Serving {place.cuisines}
+              </h4>
             </div>
             <div>
               <h1>Comments</h1>
               <p className="text-center">
                 No comments yet!
               </p>
+            </div>
             </div>
             <a href={`/places/${id}/edit`} className="btn btn-warning"> 
               Edit
