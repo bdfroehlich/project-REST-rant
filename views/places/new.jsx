9259e3e45e2ec63e739ea95f86615278a9ab2a87
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-03-09 16:42:28 -0600

    added validations and validation error on front-end

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,13 +1,22 @@
 const React = require('react')
 const Default = require('../default')
 
-function New () {
+function New (data) {
+    let message = ''               
+    if (data.message) {
+      message = (
+        <h4 className="alert-danger">
+          {data.message}
+        </h4>
+      )
+    }
     return (
         <Default>
             <main>
                 <h1>
                     Add a New Place
                 </h1>
+                {message}
                 <form method="POST" action="/places">
                     <div className="form-row">
                     <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
@@ -16,15 +25,15 @@ function New () {
                     </div>
                     <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="pic">Place Picture</label>
-                        <input className="form-control" id="pic" name="pic" />
+                        <input className="form-control" id="pic" name="pic" required />
                     </div>
                     <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="city">City</label>
-                        <input className="form-control" id="city" name="city" />
+                        <input className="form-control" id="city" name="city" required />
                     </div>
                     <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="state">State</label>
-                        <input className="form-control" id="state" name="state" />
+                        <input className="form-control" id="state" name="state" required />
                     </div>
                     <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label htmlFor="cuisines">Cuisines</label>
@@ -32,7 +41,7 @@ function New () {
                     </div>
                     <div className="form-group mx-auto col-sm-6 col-md-4 col-lg-3">
                         <label for="founded">Founded Year</label>
-                        <input className="form-control" id="founded" name="founded" required />
+                        <input className="form-control" type="number" id="founded" name="founded" value={new Date().getFullYear()} required />
                     </div>
                     </div>
                 <input className="btn btn-primary" type="submit" value="Add Place" />
